9d298b55a2d3042ddf40d1972087a2c9232dd090
Tien Dang <tien140801@gmail.com>
2023-04-02 17:48:07 +0700

    Create file utter custom action

--- Bot/server.py
+++ Bot/server.py
@@ -29,6 +29,12 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
+text_import_library = f"""
+from typing import Any, Text, Dict, List
+from rasa_sdk import Action, Tracker
+from rasa_sdk.events import SlotSet
+from rasa_sdk.executor import CollectingDispatcher
+"""
 
 
 action_endpoint = EndpointConfig(url="http://localhost:5055/webhook")
@@ -40,7 +46,8 @@ http_interpreter = RasaNLUHttpInterpreter(EndpointConfig(
     },
     basic_auth=None,
 ))
-agentList = {}
+agent_list = {}
+utter_action_list = []
 
 class FileTrain:
     def __init__(self, fixed_model_name="default_model", domain=None, config=None, training_files=[]):
@@ -73,7 +80,7 @@ class {class_name}(Action):
                     confidence_of_entities[entity_name] = confidence_entity
             print("Slot of entity: ", tracker.slots[entity_name])
 
-        dispatcher.utter_template("{utter_key}", tracker, metadata={{"channel": "{channel}", "typeChat": "{type_chat}"}}, **dict(tracker.slots.items()))        
+        dispatcher.utter_message(response="{utter_key}", metadata={{"channel": "{channel}", "typeChat": "{type_chat}"}}, **dict(tracker.slots.items()))        
         return []
 """
     return action_temp
@@ -89,70 +96,101 @@ def load_all_model():
         model_path = os.path.join("models", filename)
         if os.path.exists(model_path):
             model_id =  os.path.splitext(os.path.splitext(os.path.basename(filename))[0])[0]
-            agentList[model_id] = Agent.load(model_path, action_endpoint=action_endpoint)
+            agent_list[model_id] = Agent.load(model_path, action_endpoint=action_endpoint)
 
 def train_model(model_name, domain, config, training_files, model_path):
     return train(domain=domain, config=config, training_files=training_files, output=model_path, fixed_model_name=model_name)
     
-async def create_handle_action(botId: str, pathFile: str):
+async def create_file_train(pathFile: str):
     data_file = None
+    folder_actions_location = os.path.join("actions")
+    file_actions_location = os.path.join(folder_actions_location, "actions.py")
+    if not os.path.exists(folder_actions_location):
+        os.makedirs(folder_actions_location)
+    if not os.path.exists(file_actions_location):
+        with open(file_actions_location, 'w') as buffer: 
+            buffer.write(text_import_library)
+
+    if os.path.exists(pathFile):
+        with open(pathFile, "r", encoding='utf-8') as file:
+            if file.name.endswith('domain.yml') or file.name.endswith("domain.yaml"):
+                data_file = yaml.safe_load(file)
+                if data_file is not None:
+                    with open(file_actions_location, 'a') as buffer:
+                        for key in data_file["responses"].keys():
+                            key_name = key.replace('_', ' ').title().replace(' ', '')
+                            class_name = "Action" + key_name
+                            action_name = "action_" + key 
+                            data_file["actions"].append(action_name)
+                            if action_name not in utter_action_list:
+                                utter_action_list.append(action_name)
+                                buffer.write(create_sample_action(class_name, action_name, key, "facebook", "comment"))
+
+            if file.name.endswith("stories.yml") or file.name.endswith("stories.yaml"):
+                data_file = yaml.safe_load(file)
+                if data_file is not None:
+                    for story in data_file['stories']:
+                        for step in story['steps']:
+                            for key, value in step.items():
+                                if (key == 'action' and "utter_" in value[0:6]):
+                                    step[key] = 'action_' + value
+
+            if file.name.endswith('rules.yml') or file.name.endswith("rules.yaml"):
+                print(file.name)
+                data_file = yaml.safe_load(file)
+                if data_file is not None:
+                    for rule in data_file['rules']:
+                        for step in rule['steps']:
+                            for key, value in step.items():
+                                if (key == 'action' and "utter_" in value[0:6]):
+                                    step[key] = 'action_' + value 
+    if data_file is not None: 
+        with open(pathFile, 'w') as file:
+            yaml.dump(data_file, file)
+
+def create_file_custom_action():
     text_import_library = f"""
 from typing import Any, Text, Dict, List
 from rasa_sdk import Action, Tracker
 from rasa_sdk.events import SlotSet
 from rasa_sdk.executor import CollectingDispatcher
 """
-    with open(pathFile, "r", encoding='utf-8') as file:
-        if file.name.endswith('domain.yml') or file.name.endswith("domain.yaml"):
-            data_file = yaml.safe_load(file)
-            if data_file is not None:
-                filename = "actions.py"
-                folder_location = os.path.join("uploads", botId, "actions")
-                if not os.path.exists(folder_location):
-                    os.makedirs(folder_location)
-                file_location = os.path.join(folder_location, filename)
-                with open(file_location, 'w') as buffer: 
-                    buffer.write(text_import_library)   
-                    for key in data_file['responses'].keys():
-                        key_name = key.replace("_", " ").title().replace(" ", "")
-                        class_name = 'Action' + key_name
-                        action_name = 'action_' + key 
-                        data_file["actions"].append(action_name)
-                        buffer.write(create_sample_action(class_name, action_name, key, "facebook", "comment"))
-        if file.name.endswith('stories.yml') or file.name.endswith("stories.yaml"):
-            data_file = yaml.safe_load(file)
-            if data_file is not None:
-                for story in data_file['stories']:
-                    for step in story['steps']:
-                        for key, value in step.items():
-                            if (key == 'action' and "utter_" in value[0:6]):
-                                step[key] = 'action_' + value
-        if file.name.endswith('rules.yml') or file.name.endswith("rules.yaml"):
-            print(file.name)
-            data_file = yaml.safe_load(file)
-            if data_file is not None:
-                for rule in data_file['rules']:
-                    for step in rule['steps']:
-                        for key, value in step.items():
-                            if (key == 'action' and "utter_" in value[0:6]):
-                                step[key] = 'action_' + value 
-    if data_file is None:  
-        return 
-    with open(pathFile, 'w') as file:
-        yaml.dump(data_file, file)
-    # file.truncate()
-    return 
+    folder_actions_location = os.path.join("actions")
+    if not os.path.exists(folder_actions_location):
+        os.makedirs(folder_actions_location)
+    file_actions_location = os.path.join(folder_actions_location, "actions.py")
+    with open(file_actions_location, 'w') as buffer: 
+        buffer.write(text_import_library)  
+
+    uploads_path = "./uploads"
+    folders_path = [f.path for f in os.scandir(uploads_path) if f.is_dir()]
+    for folder_path in folders_path:
+        file_domain_location = os.path.join(folder_path, "domain.yml")
+        if os.path.exists(file_domain_location):
+            with open(file_domain_location, "r", encoding='utf-8') as file:
+                data_file = yaml.safe_load(file)
+                if data_file is not None:
+                    with open(file_actions_location, 'a') as buffer: 
+                        if "responses" in data_file:
+                            for key in data_file.get("responses").keys():
+                                key_name = key.replace('_', ' ').title().replace(' ', '')
+                                class_name = "Action" + key_name
+                                action_name = "action_" + key 
+                                data_file["actions"].append(action_name)
+                                if action_name not in utter_action_list:
+                                    utter_action_list.append(action_name)
+                                    buffer.write(create_sample_action(class_name, action_name, key, "facebook", "comment"))
 
 @app.post('/train')
 async def create_upload_file(bot_id: str = Form(), files: List[Optional[UploadFile]] = File(...)):
     with concurrent.futures.ThreadPoolExecutor() as executor:
+        folder_location = os.path.join("uploads", bot_id)
+        if not os.path.exists(folder_location):
+            os.makedirs(folder_location)
         fileTrain = FileTrain(bot_id, '', '', [])    
         for file in files:
             if file.filename.endswith('.yml') or file.filename.endswith(".yaml"):
                 try:            
-                    folder_location = os.path.join("uploads", bot_id)
-                    if not os.path.exists(folder_location):
-                        os.makedirs(folder_location)
                     file_location = os.path.join(folder_location, file.filename)
                     with open(file_location, "wb") as buffer:
                         shutil.copyfileobj(file.file, buffer)
@@ -161,21 +199,21 @@ async def create_upload_file(bot_id: str = Form(), files: List[Optional[UploadFi
                             setattr(fileTrain, fileName, file_location)
                         else:
                             fileTrain.training_files.append(file_location)
-                    await create_handle_action(bot_id, file_location)
+                    await create_file_train(file_location)
                 except yaml.YAMLError as exc:
                     raise HTTPException(status_code=400, detail="Invalid YAML file") from exc
         future = executor.submit(train_model, fileTrain.fixed_model_name, fileTrain.domain, fileTrain.config, fileTrain.training_files, "models/")
         result = await asyncio.wrap_future(future)
         model_path = os.path.join("models", bot_id + ".tar.gz")
-        agentList[bot_id] = Agent.load(model_path, action_endpoint=action_endpoint)
+        agent_list[bot_id] = Agent.load(model_path, action_endpoint=action_endpoint)
         return result
         return "Successful training"
 
 @app.post('/webhooks/rasa')
 async def receive_message(request: Request):
     message = await request.json()
-    if message.get('receive_id') in agentList:
-        response = await agentList.get(message.get('receive_id')).handle_text(text_message=message.get('message'), sender_id=message.get('sender_id'))
+    if message.get('receive_id') in agent_list:
+        response = await agent_list.get(message.get('receive_id')).handle_text(text_message=message.get('message'), sender_id=message.get('sender_id'))
         print('--------------------------------')
         print("response:")
         print(response)
@@ -185,12 +223,14 @@ async def receive_message(request: Request):
                 return {"response": "Sorry, I don't understand"}
             return {"response": response[0]["text"]}
         else:
-            fallback_response = await agentList.get(message.get('receive_id')).handle_text(DEFAULT_NLU_FALLBACK_INTENT_NAME, sender_id=message.get('sender_id'))
+            fallback_response = await agent_list.get(message.get('receive_id')).handle_text(DEFAULT_NLU_FALLBACK_INTENT_NAME, sender_id=message.get('sender_id'))
             print("fallback_response: ", fallback_response)
             if len(fallback_response) == 0:
                 return {"response": "Sorry, I don't understand"}
             return {"response": fallback_response[0]["text"]}
     return {"response": f"""Model {message.get('receive_id')} not exist"""}
 
+create_file_custom_action()
+print(utter_action_list)
 load_all_model()
-print(agentList)
+print(agent_list)
