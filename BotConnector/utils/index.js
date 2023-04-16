bc414f833c4f27208955c83ca37f6c876368b87a
Tien Dang <tien140801@gmail.com>
2023-04-16 14:57:21 +0700

    Check Page Working

--- BotConnector/utils/index.js
+++ BotConnector/utils/index.js
@@ -4,8 +4,9 @@ const { WEB_PAGE_URL } = process.env;
 module.exports = {
     checkPageWorking: async (pageID) => {
         try {
-            const result = await axios.get(`${WEB_PAGE_URL}/socialTab/${pageID}/working`);
-            console.log(result);
+            const result = await axios.get(`${WEB_PAGE_URL}/socialTab/${pageID}/working`, {
+                headers: { Authorization: '5p3cti4L-t0k3n' },
+            });
             return result.data.result;
         } catch (e) {
             console.log(e.message);
