19003c6c53572bb3e3156b78e0a6f2ba4afc3059
Tien Dang <tien140801@gmail.com>
2023-04-27 00:12:06 +0700

    CRUD for agent and intent

--- Bot/server_dialogflow.py
+++ Bot/server_dialogflow.py
@@ -1,10 +1,11 @@
 from fastapi import FastAPI, UploadFile, File
 from google.oauth2 import service_account
 from google.auth.transport.requests import Request
+from google.cloud.dialogflowcx_v3 import Intent, Agent
 from google.cloud.dialogflowcx_v3 import AgentsAsyncClient, IntentsAsyncClient, FlowsAsyncClient, SessionsAsyncClient
 from google.cloud.dialogflowcx_v3 import TextInput, QueryInput, QueryParameters, TransitionRoute, UpdateFlowRequest, Fulfillment
 from google.api_core.client_options import ClientOptions
-from google.cloud.dialogflowcx_v3.types.intent import Intent
+
 from google.protobuf.field_mask_pb2 import FieldMask
 
 import fastapi
@@ -16,7 +17,7 @@ os.environ["GOOGLE_APPLICATION_CREDENTIALS"] = "./credential.json"
 
 def get_client_options(location):
     api_endpoint = "dialogflow.googleapis.com"
-    if location is not None:
+    if location is not None and not location.lower().startswith("global"):
         api_endpoint = f"{location}-dialogflow.googleapis.com"
     return ClientOptions(api_endpoint=api_endpoint)
 
@@ -41,7 +42,7 @@ async def update_flow_agent(project_id: str, location: str, agent_id: str, inten
 
     try:
         updated_flow = await flows_client.update_flow(update_request)
-        print("Flow updated: {}".format(updated_flow.name))
+        print("Flow updated successfully: {}".format(updated_flow.name))
     except Exception as ex:
         print("Error updating flow: {}".format(ex)) 
 
@@ -54,47 +55,179 @@ async def get_token(file: UploadFile = File(...)):
     credentials.refresh(Request())
     return {"access_token": credentials.token}
 
+# Intent
 @app.post("/create-intent/projects/{project_id}/locations/{location}/agents/{agent_id}")
-async def get_token(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
+async def create_intent(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
     intent_info = await request.json()
     client_options = get_client_options(location)
 
-    agents_client = AgentsAsyncClient()
     intents_client = IntentsAsyncClient(client_options=client_options)
-    project_location_parent_intent = agents_client.agent_path(project_id, location, agent_id)
+    agents_client = AgentsAsyncClient()
+    parent_intent = agents_client.agent_path(project_id, location, agent_id)
 
     intent = Intent()
     intent.display_name = intent_info.get("display_name")
     intent.description = intent_info.get("description")
     intent.priority = intent_info.get("priority")
     intent.is_fallback = intent_info.get("is_fallback") 
+    intent.training_phrases = intent_info.get("training_phrases") 
 
-    for phrase in intent_info.get("training_phrase", []):
-        training_phrase = Intent.TrainingPhrase()
-        for part in phrase.get("parts", []):
-            training_phrase.parts.append(Intent.TrainingPhrase.Part(text=part.get("text"), parameter_id=part.get("parameter_id")))
-        training_phrase.repeat_count = phrase.get("repeat_count")
-        intent.training_phrases.append(phrase)
+    # for phrase in intent_info.get("training_phrase", []):
+    #     training_phrase = Intent.TrainingPhrase()
+    #     for part in phrase.get("parts", []):
+    #         training_phrase.parts.append(Intent.TrainingPhrase.Part(text=part.get("text"), parameter_id=part.get("parameter_id")))
+    #     training_phrase.repeat_count = phrase.get("repeat_count")
+    #     intent.training_phrases.append(phrase)
 
     # parameter = Intent.Parameter()
     # parameter.entity_type = "@sys.any"
     # parameter.is_list = False
     # parameter.redact = False
     # intent.parameters.append(parameter)
-
     try:
-        new_intent = await intents_client.create_intent(parent=project_location_parent_intent, intent=intent)
+        new_intent = await intents_client.create_intent(parent=parent_intent, intent=intent)
+        print("Intent created successfully: {}".format(new_intent.name))
         await update_flow_agent(project_id, location, agent_id, new_intent.name)
-        print("Intent created: {}".format(new_intent.name))
-        return {"message": "Intent created: {}".format(new_intent.name)}
+
+        training_phrases = []
+        for phrase in new_intent.training_phrases:
+            training_phrase = {"parts": [], "repeat_count": 1}
+            for part in phrase.parts:
+                training_phrase["parts"].append({"text": part.text, "parameter_id": part.parameter_id})
+            training_phrase["repeat_count"] = phrase.repeat_count
+            training_phrases.append(training_phrase)
+
+        return {
+            "name": new_intent.name,
+            "display_name": new_intent.display_name,
+            "training_phrases": training_phrases,
+            "priority": new_intent.priority,
+        }
     except Exception as ex:
         response.status_code = get_code_ex(ex)
         print("Error creating intent: {}".format(ex)) 
         return {"message": "Error creating intent: {}".format(ex)}
 
+@app.get("/get-intent/projects/{project_id}/locations/{location}/agents/{agent_id}/intents/{intent_id}")
+async def get_intent(project_id: str, location: str, agent_id: str, intent_id: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    intents_client = IntentsAsyncClient(client_options=client_options)
+    intent_name = intents_client.intent_path(project_id, location, agent_id, intent_id)
+    try:
+        intent = await intents_client.get_intent(request={"name":intent_name})
+        training_phrases = []
+        for phrase in intent.training_phrases:
+            training_phrase = {"parts": [], "repeat_count": 1}
+            for part in phrase.parts:
+                training_phrase["parts"].append({"text": part.text, "parameter_id": part.parameter_id})
+            training_phrase["repeat_count"] = phrase.repeat_count
+            training_phrases.append(training_phrase)
+        return {
+            "name": intent.name,
+            "display_name": intent.display_name,
+            "training_phrases": training_phrases,
+            "is_fallback": intent.is_fallback,
+            "priority": intent.priority,
+            "is_fallback": intent.is_fallback,
+            "description": intent.description,
+        }
+    except Exception as ex:
+        response.status_code = get_code_ex(ex)
+        print("Error getting intent: {}".format(ex)) 
+        return {"message": "Error getting intent: {}".format(ex)}
+
+@app.get("/get-list-intent/projects/{project_id}/locations/{location}/agents/{agent_id}")
+async def get_list_intent(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    intents_client = IntentsAsyncClient(client_options=client_options)
+    agents_client = AgentsAsyncClient()
+    parent_intent = agents_client.agent_path(project_id, location, agent_id)
+    try:
+        pager = await intents_client.list_intents(request={"parent": parent_intent})
+        intents = []
+        async for page in pager.pages:
+            for intent in page.intents:
+                print(intent)
+                training_phrases = []
+                for phrase in intent.training_phrases:
+                    training_phrase = {"parts": [], "repeat_count": 1}
+                    for part in phrase.parts:
+                        training_phrase["parts"].append({"text": part.text, "parameter_id": part.parameter_id})
+                    training_phrase["repeat_count"] = phrase.repeat_count
+                    training_phrases.append(training_phrase)
+                agent_json = {
+                    "name": intent.name,
+                    "display_name": intent.display_name,
+                    "training_phrases": training_phrases,
+                    "priority": intent.priority,
+                    "is_fallback": intent.is_fallback,
+                    "description": intent.description,
+                }
+                intents.append(agent_json)
+        return intents
+    except Exception as ex:
+        response.status_code = get_code_ex(ex)
+        print("Error getting intent: {}".format(ex)) 
+        return {"message": "Error getting intent: {}".format(ex)}
+
+@app.patch("/update-intent/projects/{project_id}/locations/{location}/agents/{agent_id}/intents/{intent_id}")
+async def update_intent(project_id: str, location: str, agent_id: str, intent_id: str, request: fastapi.Request, response: fastapi.Response):
+    intent_info = await request.json()
+    client_options = get_client_options(location)
+    intents_client = IntentsAsyncClient(client_options=client_options)
+    intent_name = intents_client.intent_path(project_id, location, agent_id, intent_id)
+    try:
+        intent = await intents_client.get_intent(name=intent_name)
+        intent.display_name = intent_info.get("display_name")
+        intent.description = intent_info.get("description")
+        intent.priority = intent_info.get("priority")
+        intent.is_fallback = intent_info.get("is_fallback") 
+        intent.training_phrases = []
+        for phrase in intent_info.get("training_phrase", []):
+            training_phrase = Intent.TrainingPhrase()
+            for part in phrase.get("parts", []):
+                training_phrase.parts.append(Intent.TrainingPhrase.Part(text=part.get("text"), parameter_id=part.get("parameter_id")))
+            training_phrase.repeat_count = phrase.get("repeat_count")
+            intent.training_phrases.append(phrase)
+        updated_intent = await intents_client.update_intent(request={"intent": intent})
+        print("Intent updated successfully: {}".format(updated_intent.name))
+        await update_flow_agent(project_id, location, agent_id, updated_intent.name)
+
+        training_phrases = []
+        for phrase in updated_intent.training_phrases:
+            training_phrase = {"parts": [], "repeat_count": 1}
+            for part in phrase.parts:
+                training_phrase["parts"].append({"text": part.text, "parameter_id": part.parameter_id})
+            training_phrase["repeat_count"] = phrase.repeat_count
+            training_phrases.append(training_phrase)
+        return {
+            "name": updated_intent.name,
+            "display_name": updated_intent.display_name,
+            "training_phrases": training_phrases,
+            "priority": intent.priority,
+            "is_fallback": intent.is_fallback,
+            "description": intent.description,
+        }
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error creating agent: {}'.format(ex)) 
+        return {"message": "Error creating agent: {}".format(ex)}
+
+@app.delete("/delete-intent/projects/{project_id}/locations/{location}/agents/{agent_id}/intents/{intent_id}")
+async def delete_intent(project_id: str, location: str, agent_id: str, intent_id: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    intents_client = IntentsAsyncClient(client_options=client_options)
+    intent_name = intents_client.intent_path(project_id, location, agent_id, intent_id)
+    try:
+        await intents_client.delete_intent(request={"name": intent_name})    
+        return {"message": "Intent deleted successfully: {}".format(intent_name)}
+    except Exception as ex:
+        response.status_code = get_code_ex(ex)
+        print("Error deleting intent: {}".format(ex)) 
+        return {"message": "Error deleting intent: {}".format(ex)}
 
 @app.post("/detect-intent/projects/{project_id}/locations/{location}/agents/{agent_id}/sessions/{session_id}")
-async def get_token(project_id: str, location: str, agent_id: str, session_id: str, request: fastapi.Request, response: fastapi.Response):   
+async def detect_intent(project_id: str, location: str, agent_id: str, session_id: str, request: fastapi.Request, response: fastapi.Response):   
     message_info = await request.json()
     client_options = get_client_options(location)
 
@@ -102,7 +235,7 @@ async def get_token(project_id: str, location: str, agent_id: str, session_id: s
     session_name = sessions_client.session_path(project_id, location, agent_id, session_id)
     language_code = message_info.get("language_code") or "en"
     query_input = QueryInput(text=TextInput(text=message_info.get("text")), language_code=language_code)
-    query_params = QueryParameters(time_zone='America/New_York', analyze_query_text_sentiment=True)
+    query_params = QueryParameters(time_zone='Asia/Bangkok', analyze_query_text_sentiment=True)
 
     try:
         deteced_intent = await sessions_client.detect_intent(request={"session": session_name, "query_input": query_input, "query_params": query_params})
@@ -116,10 +249,111 @@ async def get_token(project_id: str, location: str, agent_id: str, session_id: s
             "sentiment_score": query_result.sentiment_analysis_result.score,
             "sentiment_magnitude": query_result.sentiment_analysis_result.magnitude,
         }
-        if deteced_intent is not None:
-            return result
+        return result
     except Exception as ex:        
         response.status_code = get_code_ex(ex)
         print('Error detecting intent: {}'.format(ex)) 
         return {"message": "Error detecting intent: {}".format(ex)}
 
+
+# Agent
+@app.post("/create-agent/projects/{project_id}/locations/{location}")
+async def create_agent(project_id: str, location: str, request: fastapi.Request, response: fastapi.Response):
+    agent_info = await request.json()
+    client_options = get_client_options(location)
+    agents_client = AgentsAsyncClient(client_options=client_options)
+    agent = Agent()
+    agent.display_name = agent_info.get("display_name")
+    agent.default_language_code = agent_info.get("default_language_code")
+    agent.time_zone = "Asia/Bangkok"
+    try:
+        new_agent = await agents_client.create_agent(request={"parent": f"projects/{project_id}/locations/{location}", "agent": agent})
+        return {
+            "name": new_agent.name,
+            "display_name": new_agent.display_name,
+            "default_language_code": new_agent.default_language_code,
+            "time_zone": new_agent.time_zone,
+            "start_flow": agent.start_flow,
+        }
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error creating agent: {}'.format(ex)) 
+        return {"message": "Error creating agent: {}".format(ex)}
+
+@app.get("/get-agent/projects/{project_id}/locations/{location}/agents/{agent_id}")
+async def get_agent(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    agents_client = AgentsAsyncClient(client_options=client_options)
+    agent_name = agents_client.agent_path(project_id, location, agent_id)
+    try:
+        agent = await agents_client.get_agent(request={"name":agent_name})
+        return {
+            "name": agent.name,
+            "display_name": agent.display_name,
+            "default_language_code": agent.default_language_code,
+            "time_zone": agent.time_zone,
+            "start_flow": agent.start_flow,
+        }
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error getting agent: {}'.format(ex)) 
+        return {"message": "Error getting agent: {}".format(ex)}
+
+@app.get("/get-list-agent/projects/{project_id}/locations/{location}")
+async def get_list_agent(project_id: str, location: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    agents_client = AgentsAsyncClient(client_options=client_options)
+    try:
+        pager = await agents_client.list_agents(request={"parent": f"projects/{project_id}/locations/{location}"})
+        agents = []
+        async for page in pager.pages:
+            for agent in page.agents:
+                agent_json = {
+                    "name": agent.name,
+                    "display_name": agent.display_name,
+                    "default_language_code": agent.default_language_code,
+                    "time_zone": agent.time_zone,
+                    "start_flow": agent.start_flow,
+                }
+                agents.append(agent_json)
+        return agents
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error getting list agent: {}'.format(ex)) 
+        return {"message": "Error getting list agent: {}".format(ex)}
+    
+@app.patch("/update-agent/projects/{project_id}/locations/{location}/agents/{agent_id}")
+async def update_agent(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
+    agent_info = await request.json()
+    client_options = get_client_options(location)
+    agents_client = AgentsAsyncClient(client_options=client_options)
+    agent_name = agents_client.agent_path(project_id, location, agent_id)
+    try:
+        agent = await agents_client.get_agent(name=agent_name)
+        agent.display_name = agent_info.get("display_name")
+        agent.default_language_code = agent_info.get("default_language_code")
+        agent.time_zone = agent_info.get("time_zone")
+        updated_agent = await agents_client.update_agent(request={"agent": agent})
+        return {
+            "name": updated_agent.name,
+            "display_name": updated_agent.display_name,
+            "default_language_code": updated_agent.default_language_code,
+            "time_zone": updated_agent.time_zone,
+        }
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error creating agent: {}'.format(ex)) 
+        return {"message": "Error creating agent: {}".format(ex)}
+    
+@app.delete("/delete-agent/projects/{project_id}/locations/{location}/agents/{agent_id}")
+async def delete_agent(project_id: str, location: str, agent_id: str, request: fastapi.Request, response: fastapi.Response):
+    client_options = get_client_options(location)
+    agents_client = AgentsAsyncClient(client_options=client_options)
+    agent_name = agents_client.agent_path(project_id, location, agent_id)
+    try:
+        await agents_client.delete_agent(request={"name": agent_name})    
+        return {"message": "Agent deleted successfully: {}".format(agent_name)}
+    except Exception as ex:        
+        response.status_code = get_code_ex(ex)
+        print('Error deleting agent: {}'.format(ex)) 
+        return {"message": "Error deleting agent: {}".format(ex)}
